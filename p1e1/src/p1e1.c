24855c690b68939a498cdfaace108fcd3341e349
njofre442 <njofre442@gmail.com>
2016-05-08 03:32:42 -0300

    cambio1

---
+++ p1e1/src/p1e1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : p1e1.c
+ Author      : nico
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World nico!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
