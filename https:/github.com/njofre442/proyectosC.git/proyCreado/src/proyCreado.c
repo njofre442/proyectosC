e6da74470b3b83695432f458fa5980860578c521
njofre <njofre@njofre-HP-425>
2016-05-30 00:40:44 -0300

    ahora si??

---
+++ https:/github.com/njofre442/proyectosC.git/proyCreado/src/proyCreado.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : proyCreado.c
+ Author      : Nicolas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World 12!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
